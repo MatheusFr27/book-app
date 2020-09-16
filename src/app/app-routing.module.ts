aec97b8293456d08ca972f45978644ebcb56b877
MatheusFr27 <matheusefranca1727@gmail.com>
2020-09-16 15:47:44 -0300

    Criando o componente book

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import('./modules/welcome/welcome.module').then(module => module.WelcomeModule)
   },
   {
+    path: 'books',
+    loadChildren: () => import('./modules/books/books.module').then(module => module.BooksModule)
+  },
+  {
     path: '',
     redirectTo: '/home',
     pathMatch: 'full'
