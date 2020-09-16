a80d10dba310fef77267ceae1aa33b5b15a33d56
MatheusFr27 <matheusefranca1727@gmail.com>
2020-09-15 21:06:27 -0300

    Criando o cabeçalho e importando toolbar

--- src/app/modules/navigation/navigation.module.ts
+++ src/app/modules/navigation/navigation.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation.component';
 
-
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [NavigationComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   exports: [
     NavigationComponent
