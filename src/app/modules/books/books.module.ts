e88b6436e91549376c98e111c0500dba11db97e5
MatheusFr27 <matheusefranca1727@gmail.com>
2020-09-29 14:56:01 -0300

    Importações para formulários

--- src/app/modules/books/books.module.ts
+++ src/app/modules/books/books.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { BooksRoutingModule } from './books-routing.module';
 import { BooksComponent } from './books.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { BookCardComponent } from './book-card/book-card.component';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import { BookDetailComponent } from './book-detail/book-detail.component'
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { ComponentsModule } from './../../components/components.module';
 
 @NgModule({
   declarations: [BooksComponent, BookCardComponent, BookDetailComponent],
@@ -16,7 +23,14 @@ import { BookDetailComponent } from './book-detail/book-detail.component'
     BooksRoutingModule,
     MatCardModule,
     MatButtonModule,
-    FlexLayoutModule
-  ]
+    FlexLayoutModule,
+    ComponentsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
-export class BooksModule { }
+export class BooksModule {}
